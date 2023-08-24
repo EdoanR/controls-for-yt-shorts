78676014dbe206f7914e0b601c408c534ddf460a
Zennos <edoan.rodrigues@gmail.com>
2023-08-24 18:00:22 -0300

    fix player not working when exiting shorts page

--- js/content_script.js
+++ js/content_script.js
@@ -1,16 +1,12 @@
 devLog('content script started.');
 
-let player = null;
+let PLAYER = null;
 /** @type {HTMLVideoElement} */
 let VIDEO = null;
-let listeningKeys = false;
-
-let STARTED = false;
-/** @type {HTMLElement} */
-let SHORTS_PLAYER_ELEMENT = null;
-let IS_LISTENING_KEYS = false;
+let LISTENING_KEYS = false;
 
 chrome.storage.sync.get({ enabled: true, controlAlwaysVisible: false, hideVideoInfo: false }).then(items => {
+    const playerAttributeName = 'cfyts-player';
     const playerEnabledAttributeName = 'cfyts-player-enabled';
     const alwaysVisibleAttrName = 'cfyts-controls-always-on';
     const hideVideoInfoAttrName = 'cfyts-hide-video-info';
@@ -19,19 +15,14 @@ chrome.storage.sync.get({ enabled: true, controlAlwaysVisible: false, hideVideoI
         return location.href.includes('/shorts/');
     }
 
-    if (isShortsPage()) {
-        // if already on short page, start adding the controls.
-        start();
-    } else {
-        // if not in a short page. Listen for url changes.
-        chrome.runtime.onMessage.addListener(message => {
-            if (message?.type !== 'url update') return;
-            if (!isShortsPage()) return;
-
-            // user redirected to a shorts page. start!
-            start();
-        });
-    }
+    observer.watchElements([
+        {
+            elements: ['#shorts-player'],
+            onElement: (element) => {
+                checkPage();
+            }
+        }
+    ]);
 
     // listen to storage change
     chrome.storage.sync.onChanged.addListener(changes => {
@@ -39,35 +30,47 @@ chrome.storage.sync.get({ enabled: true, controlAlwaysVisible: false, hideVideoI
         for (const key in changes) {
             items[key] = changes[key].newValue;
         }
-
+        
+        updateShortsPlayerAttributes();
+        
         if (changes['enabled']) {
-            if (!STARTED && isShortsPage() && changes['enabled'].newValue === true) {
-                start();
-            }
+            checkPage();
         }
 
-        updateShortsPlayerAttributes();
     });
 
-    function updateShortsPlayerAttributes() {
-        if (!SHORTS_PLAYER_ELEMENT) return;
+    // the current page that the script was inject could be a shorts page, so let's check it.
+    checkPage();
 
-        SHORTS_PLAYER_ELEMENT.setAttribute(playerEnabledAttributeName, items.enabled);
-        SHORTS_PLAYER_ELEMENT.setAttribute(alwaysVisibleAttrName, items.controlAlwaysVisible);
-        SHORTS_PLAYER_ELEMENT.setAttribute(hideVideoInfoAttrName, items.hideVideoInfo);
-    }
+    chrome.runtime.onMessage.addListener(message => {
+        if (message?.type !== 'url update') return;
+        // the url of the page changed, let's check if there's any youtube shorts.
+        // this is in case the observer fail on us.
 
-    async function start() {
-        if (STARTED) return; // already started.
-        if (!items.enabled) return; // extension disabled.
-        STARTED = true;
+        setTimeout(() => {
+            checkPage();
+        }, 5000);
+    });
 
-        devLog('started.');
+    function checkPage() {
+        updateShortsPlayerAttributes();
 
-        const video = await findVideo();
-        devLog('video found', video);
+        if (!isShortsPage()) {
+            if (PLAYER) PLAYER.destroy();
+            return;
+        }
 
-        player = fluidPlayer(video, {
+        const video = document.querySelector('#shorts-player video');
+
+        if (!video) return;
+        if (video.hasAttribute(playerAttributeName)) return devLog('video already has controls.');
+
+        /** @type {HTMLVideoElement} */
+        video.setAttribute(playerAttributeName, '');
+
+        VIDEO = video;
+
+        PLAYER = fluidPlayer(video, {
             layoutControls: {
                 playPauseAnimation: false,
                 playButtonShowing: false,
@@ -77,11 +80,7 @@ chrome.storage.sync.get({ enabled: true, controlAlwaysVisible: false, hideVideoI
             }
         });
 
-        SHORTS_PLAYER_ELEMENT = document.querySelector('#shorts-player');
-        updateShortsPlayerAttributes();
-
-        const fluidContainer = SHORTS_PLAYER_ELEMENT.querySelector('.fluid_controls_container');
-        devLog('fluid container:', fluidContainer);
+        const fluidContainer = document.querySelector('.fluid_controls_container');
 
         fluidContainer.addEventListener('click', e => {
             // when clicking at the video bar it can click throught it and pause/play the video.
@@ -96,55 +95,68 @@ chrome.storage.sync.get({ enabled: true, controlAlwaysVisible: false, hideVideoI
             clickMuteButton();
         });
 
-        // this is handles when user unmute the video by changing it's volume
-        let volume = video.volume;
-        let muted = video.muted;
-        video.addEventListener('volumechange', e => {
-            if (video.muted) {
-                volume = video.volume;
-                muted = true;
-            } else {
-                if (muted && video.volume !== volume) clickMuteButton();
-                muted = false;
+        // this is handle when user unmute the video by changing it's volume
+        let volume = VIDEO.volume;
+        let muted = VIDEO.muted;
+        VIDEO.addEventListener('volumechange', e => {
+            if (!VIDEO.muted && muted && VIDEO.volume !== volume) {
+                clickMuteButton();
             }
+
+            muted = VIDEO.muted;
+            volume = VIDEO.volume;
         });
 
-        document.addEventListener('keydown', e => {
-            if (!items.enabled) return;
-            if (!isShortsPage()) return;
-            if (e.target.matches('input, [contenteditable]')) return; // ignore keys on inputs.
+        if (!LISTENING_KEYS) {
+            LISTENING_KEYS = true;
+            
+            document.addEventListener('keydown', e => {
+                if (!items.enabled) return;
+                if (!isShortsPage()) return;
+                if (e.target.matches('input, [contenteditable]')) return;
+    
+                const preventDefault = () => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    e.stopImmediatePropagation();
+                }
+    
+                if (e.shiftKey) return; // ignore shorcuts using shift key.
 
-            const preventDefault = () => {
-                e.preventDefault();
-                e.stopPropagation();
-                e.stopImmediatePropagation();
-            }
+                // spacebar and "M" are already handled by YouTube.
+                if (e.key === 'ArrowUp') {
+                    VIDEO.volume = Math.min(1, VIDEO.volume + 0.05);
+                    preventDefault();
+                } else if (e.key === 'ArrowDown') {
+                    VIDEO.volume = Math.max(0, VIDEO.volume - 0.05);
+                    preventDefault();
+                } else if (e.key === 'ArrowLeft') {
+                    VIDEO.currentTime = Math.max(VIDEO.currentTime - 5, 0);
+                    preventDefault();
+                } else if (e.key === 'ArrowRight') {
+                    VIDEO.currentTime = Math.min(VIDEO.currentTime + 5, VIDEO.duration);
+                    preventDefault();
+                } else if (e.key.match(/^[0-9]$/)) {
+                    const numericValue = parseInt(e.key);
+                    const percentage = numericValue * 10;
+                    const position = (VIDEO.duration * percentage) / 100;
+    
+                    VIDEO.currentTime = position;
+                    preventDefault();
+                }
+    
+            }, true);
+        }
 
-            if (e.shiftKey) return; // ignore shorcuts using shift key.
+    }
 
-            // spacebar and "M" are already handled by YouTube.
-            if (e.key === 'ArrowUp') {
-                video.volume = Math.min(1, video.volume + 0.05);
-                preventDefault();
-            } else if (e.key === 'ArrowDown') {
-                video.volume = Math.max(0, video.volume - 0.05);
-                preventDefault();
-            } else if (e.key === 'ArrowLeft') {
-                video.currentTime = Math.max(video.currentTime - 5, 0);
-                preventDefault();
-            } else if (e.key === 'ArrowRight') {
-                video.currentTime = Math.min(video.currentTime + 5, video.duration);
-                preventDefault();
-            } else if (e.key.match(/^[0-9]$/)) {
-                const numericValue = parseInt(e.key);
-                const percentage = numericValue * 10;
-                const position = (video.duration * percentage) / 100;
+    function updateShortsPlayerAttributes() {
+        const shortsPlayer = document.querySelector('#shorts-player');
+        if (!shortsPlayer) return;
 
-                video.currentTime = position;
-                preventDefault();
-            }
-
-        }, true);
+        shortsPlayer.setAttribute(playerEnabledAttributeName, items.enabled);
+        shortsPlayer.setAttribute(alwaysVisibleAttrName, items.controlAlwaysVisible);
+        shortsPlayer.setAttribute(hideVideoInfoAttrName, items.hideVideoInfo);
     }
 
     function clickMuteButton() {
@@ -152,27 +164,6 @@ chrome.storage.sync.get({ enabled: true, controlAlwaysVisible: false, hideVideoI
         const shortsMuteButton = controlButtons[1];
 
         if (shortsMuteButton) shortsMuteButton.click();
-    }
-
-    /** @returns {Promise<HTMLVideoElement>} */
-    async function findVideo() {
-        let video = document.querySelector('#shorts-container ytd-player video');
-        if (video) return video;
-
-        return new Promise(resolve => {
-            // watch for the video player element.
-            observer.watchElements([
-                {
-                    elements: ['.video-stream.html5-main-video'],
-                    onElement: element => {
-                        if (video) return;
-
-                        video = document.querySelector('#shorts-container ytd-player video');
-                        if (video) resolve(video);
-                    }
-                }
-            ])
-        });
     }
 });
 
