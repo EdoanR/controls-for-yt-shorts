e6066551aea25fe20d5c0cf71e21a5715844f733
Edoan <edoan.rodrigues@gmail.com>
2025-04-30 14:49:50 -0300

    control volume with scroll wheel

--- js/player.js
+++ js/player.js
@@ -375,7 +375,7 @@ class YTShortsPlayer {
             <path class="ytp-svg-fill" d="m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z"></path>
           </svg>
         </button>
-        <div class="volume-slider">
+        <div class="volume-slider center-slider">
           <input class="slider" type="range" value="0" min="0" max="100"/>
         </div>
       `;
@@ -387,6 +387,9 @@ class YTShortsPlayer {
       if (isExtensionDisabledOrReloaded()) return;
       this.shortsMuteButton.click();
     });
+
+    /** @type {HTMLDivElement} */
+    const volumeSliderContainer = volumeControl.querySelector('.volume-slider');
 
     const volumeSlider = volumeControl.querySelector(
       '.volume-slider input[type="range"]',
@@ -455,6 +458,22 @@ class YTShortsPlayer {
     volumeSlider.addEventListener('input', (e) => {
       if (isExtensionDisabledOrReloaded()) return;
       this.shortsVolumeSlider.value = volumeSlider.value;
+      this.shortsVolumeSlider.dispatchEvent(new Event('input'));
+      updateVolumeSliderBackground();
+    });
+
+    // Change volume by scrolling on the volume slider while hovering it.
+    // We do this on the volume container because it's a bigger area to scroll into it.
+    volumeSliderContainer.addEventListener('wheel', (e) => {
+      if (isExtensionDisabledOrReloaded()) return;
+      e.preventDefault();
+      
+      const currentValue = parseInt(volumeSlider.value);
+      const delta = e.deltaY > 0 ? -5 : 5;
+      const newValue = Math.max(0, Math.min(100, currentValue + delta));
+      
+      volumeSlider.value = newValue;
+      this.shortsVolumeSlider.value = newValue;
       this.shortsVolumeSlider.dispatchEvent(new Event('input'));
       updateVolumeSliderBackground();
     });
